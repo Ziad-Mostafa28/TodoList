69719378cca46939fbe6f7c5d58bc7470cbd5417
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-08-10 13:33:18 +0300

    add new item

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
       {id:1,name:'hamza',age:22},
       {id:2,name:'ahmed',age:21},
       {id:3,name:'abdo',age:20},
+      {id:4,name:'ali',age:56},
     ]
   }
   render () {
