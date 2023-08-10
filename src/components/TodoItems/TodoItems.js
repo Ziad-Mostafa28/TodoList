cb0eddc651a36f9868099411b50174aa0cbc372a
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-08-10 17:54:43 +0300

    no edit

--- src/components/TodoItems/TodoItems.js
+++ src/components/TodoItems/TodoItems.js
@@ -1,9 +1,29 @@
 import React from "react";
+import "./TodoItem.css"
+const TodoItemss = (props) => {
+    const {items , deleteItem } = props;
+    let length =items.length
+    const ListItems = length ? (items.map(item => {
+        return (
+            <div key={item.id}>
+                <span className="name">{item.name}</span>
+                <span className="age">{item.age}</span>
+                <span className="action icon" onClick={() => {deleteItem(item.id)}}>&times;</span>
+            </div>
+        )
+    })
+    ) : (
+        <p> there is no item </p>
+    )
 
-const TodoItemss = () => {
     return (
-        <div>
-            Todo listttt
+        <div className="ListItems">
+            <div>
+                <span className="name title">Name</span>
+                <span className="age title">Age</span>
+                <span className="action icon">Action</span>
+            </div>
+            {ListItems}
         </div>
     )
 }
