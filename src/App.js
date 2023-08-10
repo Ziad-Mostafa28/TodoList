f45502361fe41c66b92e808b9f5a85fc66a3f44f
Ziad-Mostafa28 <ziad46363@gmail.com>
2023-08-10 17:54:02 +0300

    finally

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import TodoItemss from "./components/TodoItems/TodoItems";
+import AddItemsss from "./components/AddItems/AddItem";
 
 
 class App extends Component {
   state ={
-    item:[
+    items:[
       {id:1,name:'hamza',age:22},
       {id:2,name:'ahmed',age:21},
       {id:3,name:'abdo',age:20},
@@ -12,13 +13,28 @@ class App extends Component {
       {id:5,name:'mohamed',age:789},
     ]
   }
+
+  deleteItem = (id) => {
+    let items = this.state.items.filter(items => {
+      return items.id !== id 
+    })
+    this.setState({items});
+  }
+
+  addItem = (item) => {
+    item.id = Math.random();
+    let items= this.state.items;
+    items.push(item);
+    this.setState ({items});
+  }
   render () {
   return (
-    <div className="App">
-      Todo App
-      <TodoItemss/>
+    <div className="App container">
+      <h1 className="text-center">Todo App</h1>
+      <TodoItemss items = {this.state.items} deleteItem = {this.deleteItem} />
+      <AddItemsss addItem={this.addItem}/>
     </div>
-  );
+  ); 
   
 }
 }
